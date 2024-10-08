4685930dff0adc5170eb9249446e075f377a71fc
CRMDosp <cristobal.melo@gmail.com>
2024-10-08 13:26:08 -0300

    Generacion BitStream
    Generacion del bitstream para poder solucionar el problema de oneshoot (creemos que es el culpable de que las cuentas en el fpga no se actualicen)

--- FPGA-Vivado-Project/Genesys2_VideoDemoR1/Genesys2_VideoDemoR1.ip_user_files/ip/mig_7series_0/mig_7series_0_stub.v
+++ FPGA-Vivado-Project/Genesys2_VideoDemoR1/Genesys2_VideoDemoR1.ip_user_files/ip/mig_7series_0/mig_7series_0_stub.v
@@ -2,7 +2,7 @@
 // Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
-// Date        : Tue Sep 24 12:03:38 2024
+// Date        : Tue Oct  8 13:17:50 2024
 // Host        : LAPTOP-RR96MLV1 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               c:/Users/crist/OneDrive/Documentos/GitHub/FPGA-UBB-project/FPGA-Vivado-Project/Genesys2_VideoDemoR1/Genesys2_VideoDemoR1.gen/sources_1/ip/mig_7series_0/mig_7series_0_stub.v
