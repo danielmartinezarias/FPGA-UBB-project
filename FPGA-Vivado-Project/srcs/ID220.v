63d8e289b66d07d491895fe450135dfa16b126df
CRMDosp <cristobal.melo@gmail.com>
2024-10-25 16:49:16 -0300

    Solucion Error AccCuentas
    Encontre algunos errores en la AccCuentas que arregle, faltaba declarar algunas variables

--- FPGA-Vivado-Project/srcs/ID220.v
+++ FPGA-Vivado-Project/srcs/ID220.v
@@ -21,9 +21,9 @@
 
 
 module ID220(
-    input clk400MHz,
-    input trigger_in_APD,
-    input [31:0] width_ID220,delay_ID220,
+    input wire clk400MHz, // el original es input clk400MHz, creo que hace falta agregar un wire al igual para trigger y width (edit: CMD)
+    input wire trigger_in_APD,
+    input wire [31:0] width_ID220,delay_ID220,
     output reg gate_ID220 = 1'b0
     );
     
@@ -31,13 +31,13 @@ module ID220(
     reg [31:0] delay_reg = 32'd0;
     reg [31:0] count = 32'd0;
     reg [31:0] count_width = 32'd0;
-    
+    wire os_trigger_in_APD;               // Definimos como wire os_trigger ya que no estba definido (edit: CMD)
     reg [1:0]state = 0; 
     
     OneShoot o1 (
     .clk(clk400MHz), 
     .signal(trigger_in_APD), 
-    .trigger(os_trigger_in_APD)
+    .trigger(os_trigger_in_APD) // os trigger in apd no está definida la agregaré más arriba (edit: CMD)
     );
     
     always@(posedge clk400MHz)begin
